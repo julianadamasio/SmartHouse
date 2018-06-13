a5158c79ca3deddf9d3c9e3c9c9b6c32568ead8c
Juliana <Juliana@192.168.1.139>
2018-06-13 16:26:24 -0300

    dispositivos alterações

--- smartHome/src/env/tools/Dispositivos.java
+++ smartHome/src/env/tools/Dispositivos.java
@@ -6,21 +6,30 @@ import cartago.*;
 
 public class Dispositivos extends Artifact {
 
-	void init(String tipoDispositivo, int status) {
+	void init(String tipoDispositivo, int status, int timeOn, int timeOff) {
 		defineObsProperty("tipoDispositivo", tipoDispositivo);
 		defineObsProperty("status", status);
+		defineObsProperty("timeOn", timeOn);
+		defineObsProperty("timeOff", timeOff);
 	}
 
 	@OPERATION
-	void operaDispositivo(ArtifactId idArt) {
-
+	void operaDispositivo() {
 		//ObsProperty tipoD = getObsProperty("tipoDispositivo");
 		ObsProperty statusD = getObsProperty("status");
-
+		
+		//se for desligado, liga
+		// se for ligado, desliga
 		if(statusD.intValue() == 0){
-			signal("liga");
+			statusD.updateValue(1);
+			System.out.println("[ArtDispositivos] Ligou");
+			//fazer print
+			//signal("liga");
 		}else{
-			signal("desliga");
+			statusD.updateValue(0);
+			System.out.println("[ArtDispositivos] Desligou");
+			//fazer print
+			//signal("desliga");
 		}
 
 	}
