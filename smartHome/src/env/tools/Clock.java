2bcd7cf445007cc11440e93a570020ea65b7a9b5
Juliana <Juliana@192.168.1.139>
2018-06-14 16:11:16 -0300

    Clock funfando

--- smartHome/src/env/tools/Clock.java
+++ smartHome/src/env/tools/Clock.java
@@ -1,41 +1,76 @@
 // CArtAgO artifact code for project smartHome
 
+
 package tools;
 
-import cartago.*;
+
+import java.util.logging.Logger;
+
+
+import cartago.Artifact;
+
+import cartago.INTERNAL_OPERATION;
+
+import cartago.OPERATION;
+
+import cartago.ObsProperty;
+
 
 public class Clock extends Artifact {
-	  boolean counting;
-	  final static long TICK_TIME = 1000;
-	  int hora = 0;
 
-	  void init(){
-	    counting = false;
-	  }
+	private int valorHora = 0;
+	private Logger logger = Logger.getLogger("clock."+Clock.class.getName());
+	boolean counting;
+	final static long TICK_TIME = 5000;
 
-	  @OPERATION void start(){
-	    if (!counting){
-	      counting = true;
-	      execInternalOp("count");
-	    } else {
-	      failed("already_counting");
-	    }
-	  }
+	void init(){
+		//valorHora = 0;
+		counting = false;
+		execInternalOp("start");
+		//logger.info("Entrou no init");
+	}
 
-	  @OPERATION void stop(){
-	    counting = false;
-	  }
 
-	  @INTERNAL_OPERATION void count(){
-	    while (counting){
-	      signal("tick");
-	      await_time(TICK_TIME);
-	    }
-	    
-	   // @OPERATION int valida(int h){
-	    //	if()
-	    	
-	    //}
-	  }
+	@OPERATION 
+	void start(){
+	   //logger.info("Entrou no start");
+		if (!counting){
+			counting = true;
+			execInternalOp("count");
+		} else {
+			failed("already_counting");
+		}
+	}
+
+	@OPERATION 
+	void stop(){
+		counting = false;
+	}
+
+	@INTERNAL_OPERATION 
+	void count(){
+		//logger.info("Entrou no count");
+		while (counting){
+			//hora = valida24h(valorHora);
+			await_time(TICK_TIME);
+			signal("tick", valorHora);
+			valida24h(valorHora);
+			//logger.info("count Tick e:"+ valorHora);
+		} 
+	}
+
+
+	@OPERATION
+	int valida24h(int valorH){
+		//logger.info("Entrou no 24h" + valorH);
+		if(valorH<23){
+			valorHora= valorH + 1;   
+		}else{
+			valorHora=0;
+		}    
+		//logger.info("valida24 Tick e atualizado:"+ valorHora);
+		return valorHora;
+	}
+
+
 }
-
